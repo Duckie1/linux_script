28282244565b00cd17a71dd1c3003e8a5b87ad51
Duckiee <mdh8023@gmail.com>
2023-02-09 19:09:43 +0900

    Update ec2_describe.sh

--- aws_cli/ec2_describe.sh
+++ aws_cli/ec2_describe.sh
@@ -43,10 +43,6 @@ aws ec2 describe-instances --filters \
 aws ec2 describe-instances \
     --query 'Reservations[*].Instances[*].Tags[?Key==`Name`] .Value' --output text
 
-# running 상태 전체 ec2 Name, 공인 IP 출력
-aws ec2 describe-instances \
---filters "Name=instance-state-name,Values=running" \
---query "Reservations[].Instances[].[ Tags[?Key == 'Name'].Value | [0], PublicIpAddress ]" --output text
 
 #전체 ec2 전체 태그 테이블 형식으로 출력 
 aws ec2 describe-instances \
